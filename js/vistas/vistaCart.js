28566090100bdfa0642788a214582e5b7f573742
“nicolasgkruk” <nicolasgkruk@gmail.com>
2018-09-12 12:02:51 -0300

    agregado templating con remove funcionando

--- js/vistas/vistaCart.js
+++ js/vistas/vistaCart.js
@@ -52,7 +52,29 @@ VistaCart.prototype = {
     var cartList = $(".cart-list");
     cartList.html("");
     listaCart.forEach(function(product) {
-      cartList.append(`<div class="product-widget" id="${product.id}">
+      //desde aca llamamos al Template
+      var $template = $('div.product-widget');
+      // clonamos el Template
+      var $clone = $template.clone();
+      // sacamos la clase hide
+      $clone.removeClass('hide');
+      // le insertamos el texto del producto al template clonado
+      $clone.attr("id", product.id);
+      $clone.find("h3.product-name a").text(product.name);
+      $clone.find("div.product-img img").attr("src", './img/' + product.image);
+      //$clone.find("h4.product-price").text(`x ${product.cantidad} - ${product.precio} `);
+      var html = $clone.find("h4.product-price").html()
+      html = html.replace("{productName}", product.name)
+      html = html.replace("{qty}", product.cantidad)
+      html = html.replace("{price}", product.precio)
+      $clone.find("h4.product-price").html(html)
+      // agregamos el template a cart-list
+      cartList.append($clone);
+    });
+    this.configuracionDeBotones();
+  },
+
+     /*  cartList.append(`<div class="product-widget" id="${product.id}">
       <div class="product-img">
         <img src="./img/${product.image}" alt="">
       </div>
@@ -62,12 +84,17 @@ VistaCart.prototype = {
       </div>
       <button class="delete"><i class="fa fa-close"></i></button>
     </div>`);
-    });
-
-    this.configuracionDeBotones();
-  },
+    }); */
 
   configuracionDeBotones: function(){
+
+   /*  var borrar = document.getElementById("borrate");
+    if(listaCart == null || listaCart.length == 0) {
+      borrar.style.display = "none";
+    } else {
+      borrar.style.display = "block";
+    }; */
+
     var contexto = this;
 
     $("button.delete").click(function() {
